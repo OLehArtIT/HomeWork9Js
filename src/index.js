919e4826eadd33930cfbd2fe1dd9c0de83fb29e8
OlegArt <artoleg2003@gmail.com>
2023-05-01 20:04:09 +0200

    01.05.2023

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const animal = new Array ('cat', 'dog', 'snake', 'jaguar', 'monkey');
+animal.push("kiwi");
+console.log(animal);
+animal.pop()
+console.log(animal);
+delete animal[3]
+console.log(animal)
